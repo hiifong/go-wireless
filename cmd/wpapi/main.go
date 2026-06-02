12711da00bbdc43cbae8cfd40a8b0e8827132681
kyle <f@f.style>
2026-06-02 13:59:48 +0800

    Revert "remove cmd & api"
    This reverts commit a5391c9c9a0fa00a2ce7b21536edfc95a90b027e.

---
+++ cmd/wpapi/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hiifong/go-wireless/api"
+)
+
+func main() {
+	var bind string
+	flag.StringVar(&bind, "b", ":8081", "the address/port to bind to")
+	flag.Parse()
+
+	r := gin.Default()
+	api.SetupRoutes(r)
+	r.Run(bind)
+}
